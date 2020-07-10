fb2949b984310d5e9f9d6a40d0c2b909f4baa74a
Manu Alba <manualbadj@gmail.com>
2020-07-10 17:18:03 +0200

    Ejemplos de imports usando deiferentes métodos (CommonJS, ES6...)

---
+++ Laboratorio_11.2(imports)/avatar-business.js
@@ -0,0 +1,10 @@
+(function(App) {
+    App.getAvatar = function(user) {
+        const img = document.createElement("img");
+    img.src = user.avatar;
+
+    return img;
+    };
+})(window.App || (window.App = {}));
+
+
---
+++ Laboratorio_11.4(imports[ES6])/data-business.js
@@ -0,0 +1,117 @@
+function getUsers() {
+  return [
+    {
+      id: 1,
+      avatar:
+        "https://robohash.org/recusandaecumdolor.png?size=50x50&set=set1@mac.com",
+      first_name: "Morton",
+      last_name: "Bernardot",
+      email: "mbernardot0@pen.io"
+    },
+    {
+      id: 2,
+      avatar:
+        "https://robohash.org/laudantiumquamdolor.bmp?size=50x50&set=set1",
+      first_name: "Worthy",
+      last_name: "Gallon",
+      email: "wgallon1@meetup.com"
+    },
+    {
+      id: 3,
+      avatar: "https://robohash.org/recusandaeodioeum.bmp?size=50x50&set=set1",
+      first_name: "Dredi",
+      last_name: "Pimbley",
+      email: "dpimbley2@barnesandnoble.com"
+    },
+    {
+      id: 4,
+      avatar:
+        "https://robohash.org/reprehenderitdictarerum.jpg?size=50x50&set=set1",
+      first_name: "Elvin",
+      last_name: "Fitchet",
+      email: "efitchet3@kickstarter.com"
+    },
+    {
+      id: 5,
+      avatar:
+        "https://robohash.org/quamconsequuntursit.jpg?size=50x50&set=set1",
+      first_name: "Margaretha",
+      last_name: "Betz",
+      email: "mbetz4@angelfire.com"
+    },
+    {
+      id: 6,
+      avatar:
+        "https://robohash.org/doloremtemporeoccaecati.jpg?size=50x50&set=set1",
+      first_name: "Sybilla",
+      last_name: "Coon",
+      email: "scoon5@ow.ly"
+    },
+    {
+      id: 7,
+      avatar: "https://robohash.org/totamsedoptio.bmp?size=50x50&set=set1",
+      first_name: "Gert",
+      last_name: "Steane",
+      email: "gsteane6@wunderground.com"
+    },
+    {
+      id: 8,
+      avatar: "https://robohash.org/etdoloresomnis.jpg?size=50x50&set=set1",
+      first_name: "Melodie",
+      last_name: "Hartmann",
+      email: "mhartmann7@123-reg.co.uk"
+    },
+    {
+      id: 9,
+      avatar: "https://robohash.org/fugainducimus.jpg?size=50x50&set=set1",
+      first_name: "Lindsay",
+      last_name: "Wharlton",
+      email: "lwharlton8@comsenz.com"
+    },
+    {
+      id: 10,
+      avatar: "https://robohash.org/solutateneturmagni.jpg?size=50x50&set=set1",
+      first_name: "Maurice",
+      last_name: "McEwen",
+      email: "mmcewen9@geocities.com"
+    },
+    {
+      id: 11,
+      avatar: "https://robohash.org/temporaetdelectus.jpg?size=50x50&set=set1",
+      first_name: "Lanni",
+      last_name: "De Hoogh",
+      email: "ldehoogha@tamu.edu"
+    },
+    {
+      id: 12,
+      avatar: "https://robohash.org/veliteumut.jpg?size=50x50&set=set1",
+      first_name: "Janeczka",
+      last_name: "Kilfether",
+      email: "jkilfetherb@google.nl"
+    },
+    {
+      id: 13,
+      avatar:
+        "https://robohash.org/voluptassuscipitvel.jpg?size=50x50&set=set1",
+      first_name: "Colas",
+      last_name: "Redsull",
+      email: "credsullc@weather.com"
+    },
+    {
+      id: 14,
+      avatar: "https://robohash.org/repellendusdolorut.bmp?size=50x50&set=set1",
+      first_name: "Cord",
+      last_name: "Baythorp",
+      email: "cbaythorpd@cpanel.net"
+    },
+    {
+      id: 15,
+      avatar: "https://robohash.org/voluptasadin.jpg?size=50x50&set=set1",
+      first_name: "Evey",
+      last_name: "Castiglione",
+      email: "ecastiglionee@ezinearticles.com"
+    }
+  ];
+}
+
+export { getUsers };
